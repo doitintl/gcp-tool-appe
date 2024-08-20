ff904a31a65cae8ffb163b382d029943373b19db
Hannes Hayashi <26571811+hanneshayashi@users.noreply.github.com>
2024-08-20 13:50:32 +0200

    init commit (#1)
    * init commit
    
    * update README

---
+++ README.md
@@ -0,0 +1,120 @@
+# appe - Alerting Policy Price Estimator
+
+Starting January 2025, Google will begin charging for [Alerting Policies](https://cloud.google.com/monitoring/alerts).
+
+While Google has provided [documentation and examples](https://cloud.google.com/stackdriver/pricing#pricing-alerting), it is still very hard to actually estimate the cost of an Alerting Policy, let alone if you have dozens or possible hundreds of them.
+Using `appe`, you can easily estimate the price of not just an individual Alerting Policy, but all of them in your entire organization using a single command.
+
+## Quick Start
+1. Make sure you have Application Default Credentials set up correctly (See [Authentication](#authentication))
+2. Make sure you have the [recommended roles](#recommended-roles)
+3. Download the [release](https://github.com/doitintl/gcp-tool-appe/releases) for your operating system and CPU architecture
+4. Run `appe` on your project with `appe -p PROJECT_ID` (or see [usage](#usage) for other examples)
+
+## Output
+`appe` can output human-readable output to the standard console output (`stdout`) or stream the results to a CSV file while it is scanning with the `--csvOutput FILENAME` flag.
+
+## Required Permissions
+In order to get the metadata of a policy or list the existing policies within a project, you will need the following permissions:
+- `monitoring.alertPolicies.get`
+- `monitoring.alertPolicies.list`
+These would be included in the [Monitoring AlertPolicy Viewer](https://cloud.google.com/iam/docs/understanding-roles#monitoring.alertPolicyViewer) (`roles/monitoring.alertPolicyViewer`) role. However, the metadata is not enough to estimate the price and we will need to actually execute the policy’s condition. This requires the `monitoring.timeSeries.list` permission, which is included in the [Monitoring Viewer](https://cloud.google.com/iam/docs/understanding-roles#monitoring.viewer) (`roles/monitoring.viewer`) role.
+If you want to run `appe` on more than individual policies, you will also need the `resourcemanager.projects.list` permission (which is also conveniently included in the Monitoring Viewer role). If you need to recursively scan for projects (i.e. go into subfolders), you will also need the `resourcemanager.folders.list` permission.
+You can also use the `--testPermissions` flag to let `appe` verify that you have the correct permissions before trying to use them in order to avoid errors in your logs.
+
+## Recommended Roles
+We recommend that you assign the following to roles for full compatibility:
+- [Monitoring Viewer](https://cloud.google.com/iam/docs/understanding-roles#monitoring.viewer) (`roles/monitoring.viewer`)
+- [Browser](https://cloud.google.com/iam/docs/understanding-roles#browser) (`roles/browser`)
+
+## Installation
+The easiest way to get and install `appe` is to download one of the pre-compiled binaries from the [releases](https://github.com/doitintl/gcp-tool-appe/releases). `appe` is a self-contained binary without any dependencies and can be run from anywhere. You do not need to download any runtime and there is no need for an installer.
+
+## Authentication
+`appe` uses [Application Default Credentials](https://cloud.google.com/docs/authentication/application-default-credentials) (ADC) to authenticate against the various APIs.
+If you are running `appe` on a Google Cloud compute service such as Compute Engine, it will use the service's Service Account to authenticate.
+
+If you are running `appe` locally, the easiest way to set up ADC is to use [gcloud](https://cloud.google.com/sdk/gcloud/reference/auth/application-default/login) by running:
+```bash
+gcloud auth application-default login
+```
+
+See also https://cloud.google.com/docs/authentication/provide-credentials-adc#local-dev.
+
+### macOS
+Since the binary is not signed with an Apple Developer Certificate, your Mac will likely report it as untrustworthy.
+There are two ways to deal with this:
+1. Run `xattr -dr com.apple.quarantine <path to appe>` to add the executable to the allow list (credit to [openecoacoustics.org](https://openecoacoustics.org/resources/help-centre/software/unsigned/))
+2. Compile it yourself (see below)
+
+### Compile appe Yourself
+The easiest way to do this is to:
+1. [Install Go](https://go.dev/doc/install)
+2. Run `go install github.com/doitintl/gcp-tool-appe@latest`
+
+This will download the dependencies, compile the program and put the executable in your `$GOPATH/bin` directory (see [GOPATH](https://go.dev/wiki/GOPATH))
+
+Alternatively, you can also clone the repository and run `go build` to compile it manually.
+
+## Usage
+Using `appe` is fairly straightforward
+
+### Estimate the Price of Individual Policies
+To estimate the price for individual policies, you can reference them directly with the `--policy` flag:
+```bash
+./appe --policy projects/PROJECT_ID/alertPolicies/POLICY_ID
+```
+You can also specify multiple policies:
+```bash
+./appe --policy projects/PROJECT_ID/alertPolicies/POLICY_ID_1,projects/PROJECT_ID/alertPolicies/POLICY_ID_2
+```
+
+### Estimate the Price for all Policies in a Project
+To estimate the price for all policies in a project, you can specify the project either with the `--project` flag or the shorthand `-p`:
+```bash
+./appe -p PROJECT_ID
+```
+You can also specify multiple projects:
+```bash
+./appe -p PROJECT_ID_1,PROJECT_ID_2
+```
+
+### Estimate the Price for all Policies in all Projects in a Folder
+To estimate the price of all policies in all projects in a folder, you can specify the folder ID either with the `--folder` flag or the shorthand `-f`:
+```bash
+./appe -f FOLDER_ID
+```
+You can also specify multiple folders:
+```bash
+./appe -f FOLDER_ID_1,FOLDER_ID_2
+```
+Note that you will need to specify the `--recursive` or `-r` flag to also scan subfolders.
+
+### Estimate the Price for all Policies in all Projects in an Organization
+To estimate the price of all policies in all projects in an organization, you can specify the organization ID either with the `--organization` flag or the shorthand `-o`:
+```bash
+./appe -o ORG_ID
+```
+You can also specify multiple organizations:
+```bash
+./appe -o ORG_ID_1,ORG_ID_2
+```
+Note that you will need to specify the `--recursive` or `-r` flag to also scan subfolders.
+
+### All Flags
+```
+  -c, --csvOut string           Path to a CSV file to redirect output to. If this is not set, human-readable output will be given on stdout.
+  -d, --duration duration       The delta from now to go back in time for query. Default is 30 days. (default 720h0m0s)
+  -e, --excludeFolder strings   One or more folders to exclude. Separated by  ",".
+  -f, --folder strings          One or more folders to scan. Use the "-r" flag to scan recursively. Separated by ",".
+  -h, --help                    help for appe
+  -i, --includeDisabled         If the application should also include disabled policies. (default false)
+  -o, --organization strings    One or more organizations to scan. Use the "-r" flag to scan recursively. Separated by ",".
+      --policy strings          One or more alerting policies to analyze. Names must be given in full in the format "projects/PROJECT_ID/alertPolicies/POLICY_ID". Separated by ",".
+  -p, --project strings         One or more projects to scan. Separated by ",".
+  -q, --quotaProject string     A quota or billing project. Useful if you don't have the serviceusage.services.use permission in the target project.
+  -r, --recursive               If parent should be scanned recursively. If this is not set, only projects at the root of the folder or organization will be scanned. (default false)
+  -t, --testPermissions         If the application should verify that the user has the necessary permissions before processing a project. (default false)
+      --threads int             Number of threads to use to process folders, projects and policies in parallel. (default 4)
+  -v, --version                 version for appe
+```
---
+++ cmd/root.go
@@ -0,0 +1,306 @@
+package cmd
+
+import (
+	"context"
+	"encoding/csv"
+	"log"
+	"os"
+	"strconv"
+	"sync"
+	"time"
+
+	monitoring "cloud.google.com/go/monitoring/apiv3/v2"
+	"cloud.google.com/go/monitoring/apiv3/v2/monitoringpb"
+	resourcemanager "cloud.google.com/go/resourcemanager/apiv3"
+	"github.com/spf13/cobra"
+	"golang.org/x/oauth2/google"
+	"google.golang.org/api/option"
+	"google.golang.org/protobuf/types/known/timestamppb"
+)
+
+// rootCmd represents the base command when called without any subcommands
+var rootCmd = &cobra.Command{
+	Version: "0.1",
+	Use:     "appe",
+	Short:   "Alerting Policy Price Estimator",
+	Long:    `Scans for alerting policies in the specified projects, folder or orgs and approximates their cost by executing the queries defined in them against the monitoring API`,
+	Run:     func(cmd *cobra.Command, args []string) {},
+	Example: `To estimate the price for individual policies, you can reference them directly with the --policy flag:
+./appe --policy projects/PROJECT_ID/alertPolicies/POLICY_ID
+You can also specify multiple policies:
+./appe --policy projects/PROJECT_ID/alertPolicies/POLICY_ID_1,projects/PROJECT_ID/alertPolicies/POLICY_ID_2
+
+To estimate the price for all policies in a project, you can specify the project either with the --project flag or the shorthand -p:
+./appe -p PROJECT_ID
+You can also specify multiple projects:
+./appe -p PROJECT_ID_1,PROJECT_ID_2
+
+./appe -f FOLDER_ID
+You can also specify multiple folders:
+./appe -f FOLDER_ID_1,FOLDER_ID_2
+
+To estimate the price of all policies in all projects in an organization, you can specify the organization ID either with the --organization flag or the shorthand -o:
+./appe -o ORG_ID
+You can also specify multiple organizations:
+./appe -o ORG_ID_1,ORG_ID_2
+
+Note that you will need to specify the --recursive or -r flag to also scan subfolders.`,
+}
+
+// Execute adds all child commands to the root command and sets flags appropriately.
+// This is called by main.main(). It only needs to happen once to the rootCmd.
+func Execute() {
+	err := rootCmd.Execute()
+	if err != nil {
+		os.Exit(1)
+	}
+
+	// Parse flags
+	projects, err := rootCmd.Flags().GetStringSlice("project")
+	if err != nil {
+		log.Fatalln(err)
+	}
+	folders, err := rootCmd.Flags().GetStringSlice("folder")
+	if err != nil {
+		log.Fatalln(err)
+	}
+	organizations, err := rootCmd.Flags().GetStringSlice("organization")
+	if err != nil {
+		log.Fatalln(err)
+	}
+	csvOut, err := rootCmd.Flags().GetString("csvOut")
+	if err != nil {
+		log.Fatalln(err)
+	}
+	threads, err := rootCmd.Flags().GetInt64("threads")
+	if err != nil {
+		log.Fatalln(err)
+	}
+	recursive, err := rootCmd.Flags().GetBool("recursive")
+	if err != nil {
+		log.Fatalln(err)
+	}
+	testPermissions, err := rootCmd.Flags().GetBool("testPermissions")
+	if err != nil {
+		log.Fatalln(err)
+	}
+	includeDisabled, err := rootCmd.Flags().GetBool("includeDisabled")
+	if err != nil {
+		log.Fatalln(err)
+	}
+	quotaProject, err := rootCmd.Flags().GetString("quotaProject")
+	if err != nil {
+		log.Fatalln(err)
+	}
+	duration, err := rootCmd.Flags().GetDuration("duration")
+	if err != nil {
+		log.Fatalln(err)
+	}
+	excludedFolders, err := rootCmd.Flags().GetStringSlice("excludeFolder")
+	if err != nil {
+		log.Fatalln(err)
+	}
+	policies, err := rootCmd.Flags().GetStringSlice("policy")
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	// Set up re-usable variables
+	ctx := context.Background()
+	now := time.Now()
+	end := timestamppb.Now()
+	start := timestamppb.New(now.Add(-duration))
+	projectsIn := make(chan string, threads)
+	projectsTested := make(chan string, threads)
+	policiesIn := make(chan *monitoringpb.AlertPolicy, threads)
+	policiesOut := make(chan *policy, threads)
+	lenP := len(projects)
+	lenF := len(folders)
+	lenO := len(organizations)
+	lenPol := len(policies)
+
+	// Set up API clients
+	alertingPolicyClient, err := monitoring.NewAlertPolicyClient(ctx, option.WithQuotaProject(quotaProject))
+	if err != nil {
+		log.Fatalf("Failed to create alert policy client: %v", err)
+	}
+	queryClient, err := monitoring.NewQueryClient(ctx, option.WithQuotaProject(quotaProject))
+	if err != nil {
+		log.Fatalf("Failed to create query client: %v", err)
+	}
+	metricClient, err := monitoring.NewMetricClient(ctx, option.WithQuotaProject(quotaProject))
+	if err != nil {
+		log.Fatalf("Failed to create metric client: %v", err)
+	}
+	projectsClient, err := resourcemanager.NewProjectsClient(ctx, option.WithQuotaProject(quotaProject))
+	if err != nil {
+		log.Fatalf("Failed to create projects client: %v", err)
+	}
+	foldersClient, err := resourcemanager.NewFoldersClient(ctx, option.WithQuotaProject(quotaProject))
+	if err != nil {
+		log.Fatalf("Failed to create folders client: %v", err)
+	}
+	// https://github.com/googleapis/google-api-go-client/issues/2304
+	// monitoring_v1Service, err := monitoring_v1.NewService(ctx, option.WithQuotaProject(quotaProject))
+	// if err != nil {
+	// 	log.Fatalf("Failed to create monitoring v1 client: %v", err)
+	// }
+	httpClient, err := google.DefaultClient(ctx, monitoring.DefaultAuthScopes()...)
+	if err != nil {
+		log.Fatalf("Failed to create default http client: %v", err)
+	}
+
+	// If the application was executed with the --project or -p flag, put all the projects directly in the projects channel.
+	// Once done, we close the projects channel because we know there won't be any more projects coming in.
+	if lenP > 0 {
+		if lenP > int(threads) {
+			threads = int64(lenP)
+		}
+		go func() {
+			for i := range projects {
+				projectsIn <- projects[i]
+			}
+			close(projectsIn)
+		}()
+	}
+
+	// If the application was executed with orgs or folders, we first list the parents under them.
+	// Once done, we close the projects channel because we know there won't be any more projects coming in.
+	if lenF > 0 {
+		go func() {
+			for i := range folders {
+				listProjects(ctx, projectsClient, foldersClient, "folders/"+folders[i], projectsIn, recursive, excludedFolders)
+			}
+			close(projectsIn)
+		}()
+	}
+	if lenO > 0 {
+		go func() {
+			for i := range organizations {
+				listProjects(ctx, projectsClient, foldersClient, "organizations/"+organizations[i], projectsIn, recursive, excludedFolders)
+			}
+			close(projectsIn)
+		}()
+	}
+
+	// If one or more individual policies should be analyzed, we need to first get them from the API.
+	// We then put them directly on the policiesIn channel, which will be processes by threads that are spawned below.
+	// Finally, we will close teh projectsIn channel once done, because the policiesIn channel will be closed automatically.
+	if lenPol > 0 {
+		if lenPol > int(threads) {
+			threads = int64(lenPol)
+		}
+		go func() {
+			for i := range policies {
+				policy, err := alertingPolicyClient.GetAlertPolicy(ctx, &monitoringpb.GetAlertPolicyRequest{
+					Name: policies[i],
+				})
+				if err != nil {
+					log.Fatal(err)
+				}
+				policiesIn <- policy
+			}
+			close(projectsIn)
+		}()
+	}
+
+	// We create a wait group with the number of threads to use for parallel processing of projects
+	// We then spawn the threads that will verify the permissions on the projects and put them in the projectsTested channel
+	var wg1 sync.WaitGroup
+	wg1.Add(int(threads))
+	for i := 0; i < int(threads); i++ {
+		go func() {
+			for project := range projectsIn {
+				verifyProjectPermissions(ctx, projectsClient, project, projectsTested, testPermissions)
+			}
+			wg1.Done()
+		}()
+	}
+
+	// We create a second wait group with the number of threads to use for parallel processing of projects
+	// We then create the threads that will look for policies in the tested projects and put them in the policiesIn channel
+	var wg2 sync.WaitGroup
+	wg2.Add(int(threads))
+	for i := 0; i < int(threads); i++ {
+		go func() {
+			for project := range projectsTested {
+				// processAlertingPolicies(ctx, alertingPolicyClient, queryClient, metricClient, httpClient, project, start, end, parallelPolicies, policiesOut)
+				listAlertPolicies(ctx, project, includeDisabled, alertingPolicyClient, policiesIn)
+			}
+			wg2.Done()
+		}()
+	}
+
+	// These threads will loop over the found policies and execute their queries to estimate their cost
+	var wg3 sync.WaitGroup
+	wg3.Add(int(threads))
+	for i := 0; i < int(threads); i++ {
+		go func() {
+			for policy := range policiesIn {
+				processAlertPolicy(ctx, queryClient, metricClient, httpClient, policy, start, end, policiesOut)
+			}
+			wg3.Done()
+		}()
+	}
+
+	// We create one thread that will just wait for the other threads and close the channels in the correct order
+	go func() {
+		// We wait until all of the threads that may put projects in the projectsTested channel are done before closing it
+		wg1.Wait()
+		close(projectsTested)
+		// We then wait until all of the threads that are listing policies are done before closing the policiesIn channel
+		wg2.Wait()
+		close(policiesIn)
+		// We then wait until all of the threads that are processing policies are done before closing the policiesOut channel
+		wg3.Wait()
+		close(policiesOut)
+	}()
+
+	// If the --csvOut flag was used, we create a CSV writer and write each policy as a line to the file
+	if csvOut != "" {
+		csvFile, err := os.Create(csvOut)
+		if err != nil {
+			log.Fatalf("Failed to create CSV file: %v", err)
+		}
+		defer csvFile.Close()
+		csvWriter := csv.NewWriter(csvFile)
+		err = csvWriter.Write([]string{"ProjectId", "Policy Name", "DisplayName", "Conditions", "Time Series", "Price", "Error"})
+		if err != nil {
+			log.Fatalln("Failed writing header to file", err)
+		}
+		csvWriter.Flush()
+		for policy := range policiesOut {
+			err = csvWriter.Write([]string{policy.ProjectId, policy.Name, policy.DisplayName, strconv.Itoa(policy.Conditions), strconv.Itoa(policy.TimeSeries), strconv.FormatFloat(policy.Price, 'f', 2, 64), policy.Error})
+			if err != nil {
+				log.Fatalln("Failed writing record to file", err)
+			}
+			csvWriter.Flush()
+		}
+		// Otherwise, the application will just output to stdout
+	} else {
+		for policy := range policiesOut {
+			log.Printf("Alerting Policy %s (%s) has %d condition(s) and %d time series. It will cost approximately $%f\n", policy.DisplayName, policy.Name, policy.Conditions, policy.TimeSeries, policy.Price)
+		}
+	}
+}
+
+func init() {
+	rootCmd.Flags().StringP("quotaProject", "q", "", "A quota or billing project. Useful if you don't have the serviceusage.services.use permission in the target project.")
+	rootCmd.Flags().StringP("csvOut", "c", "", "Path to a CSV file to redirect output to. If this is not set, human-readable output will be given on stdout.")
+	rootCmd.Flags().StringSlice("policy", nil, "One or more alerting policies to analyze. Names must be given in full in the format \"projects/PROJECT_ID/alertPolicies/POLICY_ID\". Separated by \",\".")
+	rootCmd.Flags().StringSliceP("project", "p", nil, "One or more projects to scan. Separated by \",\".")
+	rootCmd.Flags().StringSliceP("folder", "f", nil, "One or more folders to scan. Use the \"-r\" flag to scan recursively. Separated by \",\".")
+	rootCmd.Flags().StringSliceP("organization", "o", nil, "One or more organizations to scan. Use the \"-r\" flag to scan recursively. Separated by \",\".")
+	rootCmd.Flags().StringSliceP("excludeFolder", "e", nil, "One or more folders to exclude. Separated by  \",\".")
+	rootCmd.Flags().BoolP("testPermissions", "t", false, "If the application should verify that the user has the necessary permissions before processing a project. (default false)")
+	rootCmd.Flags().BoolP("includeDisabled", "i", false, "If the application should also include disabled policies. (default false)")
+	rootCmd.Flags().BoolP("recursive", "r", false, "If parent should be scanned recursively. If this is not set, only projects at the root of the folder or organization will be scanned. (default false)")
+	rootCmd.Flags().Int64("threads", 4, "Number of threads to use to process folders, projects and policies in parallel.")
+	rootCmd.Flags().DurationP("duration", "d", 24*time.Hour*30, "The delta from now to go back in time for query. Default is 30 days.")
+	rootCmd.MarkFlagsOneRequired("policy", "project", "folder", "organization")
+	rootCmd.MarkFlagsMutuallyExclusive("policy", "project", "recursive")
+	rootCmd.MarkFlagsMutuallyExclusive("policy", "testPermissions")
+	rootCmd.MarkFlagsMutuallyExclusive("policy", "includeDisabled")
+	rootCmd.MarkFlagsMutuallyExclusive("policy", "project", "excludeFolder")
+	rootCmd.MarkFlagsMutuallyExclusive("policy", "project", "folder", "organization")
+}
